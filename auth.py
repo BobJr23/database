8e09cb2c84ab2f3897e213e9cced1752c201d2c7
Andrew Ma <ama25@belmontschools.net>
2024-08-02 17:00:31 -0400

    continuing auth

--- auth.py
+++ auth.py
@@ -1,17 +1,32 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
+HashedPassword = os.getenv("HASH_PASS")
 
-db = {"username": "test", "password": "", "email": "test@gmail.com", "name": "Joe"}
+db = {
+    "test": {
+        "username": "test",
+        "password": "bot",
+        "email": "test@gmail.com",
+        "name": "Joe",
+        "hashed_password": HashedPassword,
+        "disabled": False,
+    }
+}
 
 
+# from fastapi docs
 class Data(BaseModel):
     username: str
     password: str
@@ -40,5 +55,70 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def check_password(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    print(username, db)
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    print(user.hashed_password)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except Exception as e:
+        print(e)
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+if __name__ == "__main__":
+    print(get_password_hash("bot"))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from uuid import UUID, uuid4
 import testdb
 from pydantic import BaseModel
-import auth
+from auth import *
 
 app = FastAPI()
 
@@ -33,6 +33,24 @@ async def create_student(student: Student):
 async def get_students():
     students = testdb.get_students()
     return students
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 
 if __name__ == "__main__":
